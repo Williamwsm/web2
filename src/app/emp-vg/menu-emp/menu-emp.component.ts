656a91239c888a54dbc764fb5e169421696d30d2
will <113859999+Williamwsm@users.noreply.github.com>
2025-02-21 22:05:25 -0300

    add menu-perfil

--- src/app/emp-vg/menu-emp/menu-emp.component.ts
+++ src/app/emp-vg/menu-emp/menu-emp.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { MenuPerfilComponent } from "../../login/menu/menu-perfil/menu-perfil.component";
 
 @Component({
   selector: 'app-menu-emp',
   standalone: true,
-  imports: [],
+  imports: [MenuPerfilComponent],
   templateUrl: './menu-emp.component.html',
   styleUrl: './menu-emp.component.css'
 })
--- src/app/portal-vg/menu-pricipal/menu-pricipal.component.ts
+++ src/app/portal-vg/menu-pricipal/menu-pricipal.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { MenuPerfilComponent } from "../../login/menu/menu-perfil/menu-perfil.component";
 
 @Component({
   selector: 'app-menu-pricipal',
   standalone: true,
-  imports: [],
+  imports: [MenuPerfilComponent],
   templateUrl: './menu-pricipal.component.html',
   styleUrl: './menu-pricipal.component.css'
 })
